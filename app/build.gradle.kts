ffc02a7a102556c081c65e00542407bc2507cd62
Alex Nou <alexnou@gmail.com>
2026-09-05 18:27:37 -0700

    Bump version to 1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.crazyfluff.shellfstudy"
         minSdk = 28
         targetSdk = 37
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
